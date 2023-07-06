adf5a21940ef6708d7915f9a7400184fe47742d5
Chaitanya Vura <vurachaitanya@users.noreply.github.com>
2023-07-06 22:21:52 +0530

    Update 1.1-comments.groovy

--- 1.1-comments.groovy
+++ 1.1-comments.groovy
@@ -3,7 +3,7 @@ println 'Hello World!!!' // a comment till the end of the line
 
 /* a standalone multiline comment
    spanning two lines */
-println 'Welcome to Kavin School' /* a multiline comment starting
+println 'Welcome to Learning School' /* a multiline comment starting
                    at the end of a statement */
 println 100 /* hundread */ + 200 /* plud two hundread */
 
